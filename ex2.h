c94c54757626df6ec36393ec53679a23715752a4
SivanJhirad <sivanush550@gamil.com>
2021-11-13 13:20:16 +0200

    version 1 - add

---
+++ ex2.h
@@ -0,0 +1,22 @@
+
+#ifndef UNTITLED2_EX2_H
+#define UNTITLED2_EX2_H
+
+typedef magnitude;
+
+#define MAX_NUMBER 2147483647;
+#define MIN_NUMBER -2147483647;
+
+
+magnitude add(magnitude a, magnitude b); // a + b
+/*
+magnitude sub(magnitude a, magnitude b); // a - b
+magnitude multi(magnitude a, magnitude b); // a * b
+
+// true = 1, false = 0
+int equal(magnitude a, magnitude b); // a = b
+int greater(magnitude a, magnitude b); // a > b
+
+*/
+#endif //UNTITLED2_EX2_H
+
